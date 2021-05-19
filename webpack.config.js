39ca1e5c39ba2777fbcd9980e8f1608f0e90ebf7
leo <lolkaazhaha@gmail.com>
2021-05-19 14:30:56 +0300

    Добавил чекбоксы

--- webpack.config.js
+++ webpack.config.js
@@ -40,12 +40,12 @@ module.exports = {
     rules: [
         // изображения
         {
-          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+          test: /\.(ico|gif|png|jpg|jpeg|svg)$/i,
           type: 'asset/resource',
         },
         // шрифты и SVG
         {
-          test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+          test: /\.(woff(2)?|eot|ttf|otf|svg)$/i,
           type: 'asset/inline',
         },
         // стили
